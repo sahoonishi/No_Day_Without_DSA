480deb02cbfa897b65bea5f4b2d292b5783ce98a
Nishi <nishisahoo293@gmail.com>
2026-03-13 00:27:58 +0530

    march 12

--- Array/Easy/RemoveDuplicateElementFromArray.java
+++ Array/Easy/RemoveDuplicateElementFromArray.java
@@ -18,6 +18,29 @@ public class RemoveDuplicateElementFromArray {
         }
     }
 
+
+
+    //-----------------------------------------TWO POINTERS APPROACH-----------------------------------------
+
+
+    
+    class Solution2 {
+        //OPTIMIZED SOLUTION O(n) TIME COMPLEXITY (1ms)(Beats 100%) AND O(1) SPACE COMPLEXITY (47MB) (Beats 60%)
+    public int removeDuplicates(int[] nums) {
+         if (nums.length == 0) return 0;
+
+        int j = 0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i] != nums[j])
+            {
+                nums[j+1] = nums[i];
+                j++;
+            }
+        }
+        return j+1;
+    }
+}
+
     public static void main(String[] args) {
         int [] arr = {1, 1, 2, 3, 4, 4, 5};
         RemoveDuplicateElementFromArray obj = new RemoveDuplicateElementFromArray();
