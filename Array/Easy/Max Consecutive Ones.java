4341e6cfcb517903c7c358d263b19cb1edbfa622
Nishi <99659309+sahoonishi@users.noreply.github.com>
2026-03-20 23:33:59 +0530

    march 18

---
+++ Array/Easy/Max Consecutive Ones.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int n = nums.length;
+        int i = -1;
+        int j = 0;
+        int count = 0;
+        
+        while (j < n) {
+            if (nums[j] != 0) {
+                j++;
+                continue;
+            }
+            
+            count = Math.max(count, j - i - 1);
+            i = j;
+            j++;
+        }
+        
+        count = Math.max(count, j - i - 1);
+        return count;
+    }
+}
